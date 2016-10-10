60b41fc1610b9bb1cf1aa221911631211982bd47
yassine kassem <yassine kassem>
2016-10-10 11:37:26 +0200

    modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-let speed = 54;
+// import pi from './modules/test';
 
-console.log(speed);
+// var test = require('./modules/test');
+
+// console.log(test.test);
+// console.log(pi);
+
+
+import { square, diag, pi, testing } from './modules/test';
+console.log(square(11)); // 121
+console.log(diag(4, 3)); // 5
+console.log(pi);
+console.log(testing);
