183221a13c8b7fb7ad53eff417cdce2f56041b5c
Tristan van der Linden <tristanvdl@gmail.com>
2016-10-31 11:18:55 +0100

    added for of loop explanation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-let speed = 54;
+var iterable = [10, 20, 30];
+iterable.name = "Enumerable property";
 
-console.log(speed);
+//OLD
+for (let value in iterable) {
+    console.log(iterable[value]);
+    //Index's zijn strings
+    //Enumerable properties worden ook meegeteld
+}
+iterable.forEach(function (value) {
+    console.log(value);
+    //Je kan geen break of return gebruiken
+    //Werkt alleen op arrays
+});
+
+//NEW
+for (let value of iterable) {
+    console.log(value);
+    //Werkt met break, continue en return
+    //Kan over bijna alles heen loopen
+}
+
+for (var chr of "😺😲") {
+    //alert(chr);
+}
